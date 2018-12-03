68f178ef8a0cca2c8bac2f3e716cf70461fba84f
MD <mduane2030@gmail.com>
2018-12-03 15:24:53 +0000

    update

--- src/main/java/com/michelle/MyUI.java
+++ src/main/java/com/michelle/MyUI.java
@@ -1,7 +1,7 @@
 package com.michelle;
-
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
-
+import java.sql.*;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -9,7 +9,7 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 import com.vaadin.ui.VerticalLayout;
 
 /**
@@ -24,7 +24,56 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        String connectionString
+        = "jdbc:sqlserver://classdb.database.windows.net:1433;"+
+        "database=classDB;"+
+        "user=michelle@classdb;"+
+        "password={Cloud18!};"+
+        "encrypt=true;"+
+        "trustServerCertificate=false;"+
+        "hostNameInCertificate=*.database.windows.net;"+
+        "loginTimeout=30;";
+
+        Connection connection = null;
         final VerticalLayout layout = new VerticalLayout();
+        try 
+{
+	// Connect with JDBC driver to a database
+	connection = DriverManager.getConnection(connectionString);
+	ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM customerTable;");
+// Convert the resultset that comes back into a List - we need a Java class to represent the data (Customer.java in this case)
+List<Customer> customers = new ArrayList<Customer>();
+// While there are more records in the resultset
+while(rs.next())
+{   
+	// Add a new Customer instantiated with the fields from the record (that we want, we might not want all the fields, note how I skip the id)
+	customers.add(new Customer(rs.getString("first_name"), 
+				rs.getString("last_name"), 
+				rs.getBoolean("paid"), 
+				rs.getDouble("amount")));
+}// Add a label to the web app with the message and name of the database we connected to 
+	//layout.addComponent(new Label("Connected to database: " + connection.getCatalog()));
+// Execute a query against the database and return rows (if any) to the ResultSet
+// Add my component, grid is templated with Customer
+Grid<Customer> myGrid = new Grid<>();
+// Set the items (List)
+myGrid.setItems(customers);
+// Configure the order and the caption of the grid
+myGrid.addColumn(Customer::getFirst_name).setCaption("Name");
+myGrid.addColumn(Customer::getLast_name).setCaption("Surname");
+myGrid.addColumn(Customer::getAmount).setCaption("Total Amount");
+myGrid.addColumn(Customer::isPaid).setCaption("Paid");
+
+// Add the grid to the list
+layout.addComponent(myGrid);
+
+} 
+catch (Exception e) 
+{
+	// This will show an error message if something went wrong
+	layout.addComponent(new Label(e.getMessage()));
+}
+setContent(layout);
         
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
